12fba5e903057a39072578697b2cfc5ac2dbd6d2
Nikos Kapsoulis <nkapsoulis@gmail.com>
2021-04-03 20:17:10 +0300

    src/

--- nfs-server.sh
+++ nfs-server.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y nfs-kernel-server
 sudo dpkg --configure -a
 
 sudo mkdir -p /local/organizations
-sudo mount --bind organizations/ /local/organizations # MUST BE GRANTED RIGHTS
+sudo mount --bind src/ /local/organizations # MUST BE GRANTED RIGHTS
 
 if [ ! -d "/local/bin" ]; then 
 	cd /local/ && curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/release-2.2/scripts/bootstrap.sh | bash -s 2.2.0 1.4.7 -ds && cd -
@@ -19,11 +19,10 @@ sudo cat /etc/exports | grep local
 if [ ! "$(cat /etc/exports | grep local)" ]; then # if NULL then
 	sudo echo '/local/bin	CLIENT_IP(rw,no_root_squash,no_subtree_check)' >> /etc/exports;
 	sudo echo '/local/organizations	CLIENT_IP(rw,no_root_squash,no_subtree_check)' >> /etc/exports;
+	sudo exportfs -a
+	sudo systemctl restart nfs-kernel-server # showmount -e CLIENT_IP
 fi
 
-sudo exportfs -a
-
-sudo systemctl restart nfs-kernel-server # showmount -e CLIENT_IP
 if [ "$(sudo ufw status | grep Status | awk '{print $2}')" == active ]; then 
 	sudo ufw allow from CLIENT_IP to any port nfs
 	sudo ufw status
