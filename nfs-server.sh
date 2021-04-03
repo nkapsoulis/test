3a555866dce591fbea5c7ff1b5f947ac6c019fdb
Nikos Kapsoulis <nkapsoulis@gmail.com>
2021-04-03 18:53:51 +0300

    apt-get

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y nfs-kernel-server
+sudo apt-get update
+sudo apt-get install -y nfs-kernel-server
 
 sudo mkdir -p /local/organizations
 sudo mount --bind organizations/ /local/organizations
